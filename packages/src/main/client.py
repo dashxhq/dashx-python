1e9f97ddd01b6291efc94f31945a1730e2a64306
Vikrant <vikrant.vishwakarma07@gmail.com>
2022-06-09 18:31:33 +0530

    Adding Client Class and Methods to create header and make http request

--- packages/src/main/client.py
+++ packages/src/main/client.py
@@ -1,3 +1,65 @@
+import json
+
+import requests
+
+from packages.src.main.constants import PUBLIC_KEY, BASE_URI, TARGET_ENV
+
+
+def create_payload(*args, **kwargs):
+    body = dict()
+    query = kwargs.get("query")
+    body["query"] = query
+    body["variables"] = dict()
+    body["variables"]["input"] = dict()
+    for params in kwargs.keys():
+        if params != "query":
+            body["variables"]["input"][params] = kwargs[params]
+    return body
+
+
 class Client:
+    __shared_instance = "HTTP Client"
+
+    pub_key = None
+    base_uri = None
+
     def __init__(self):
-        pass
+
+        if Client.__shared_instance != "HTTP Client":
+            raise Exception("This class is a singleton class !")
+        else:
+            self.pub_key = PUBLIC_KEY
+            self.base_uri = BASE_URI
+            self.target_env = TARGET_ENV
+            Client.__shared_instance = self
+
+    @staticmethod
+    def get_instance():
+
+        """Static Access Method"""
+        if Client.__shared_instance == "HTTP Client":
+            Client()
+        return Client.__shared_instance
+
+    def get_header(self):
+        return {
+            'User-Agent': 'dashx-py',
+            'X-Public-Key': self.pub_key,
+            'X-Target-Environment': self.target_env,
+            'Content-Type': 'application/json'
+        }
+
+    def make_http_req(self, req_uri, body):
+        header = self.get_header()
+        return requests.post(url=self.base_uri + req_uri, headers=header, data=body)
+
+    # def identify(self, uuid):
+    #     identity_query = create_query()
+    #     body = create_payload(query=identity_query, anonymousAccountId=uuid)
+    #     return self.make_http_req(self.base_uri, json.dumps(body))
+    #
+    # def reset(self, uuid):
+    #     identity_query = create_query()
+    #     body = create_payload(query=identity_query, anonymousAccountId=uuid)
+    #     return self.make_http_req(self.base_uri, json.dumps(body))
+
