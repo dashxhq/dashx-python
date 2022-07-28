68f0c0e6e4fe0b3d4301e4496990ca60e7d1dfa6
Vikrant <vikrant.vishwakarma07@gmail.com>
2022-07-28 08:55:14 +0530

    Fixing identify API logic, gql variable

--- src/client.py
+++ src/client.py
@@ -1,39 +1,45 @@
 import os
+
+import uuid as uuid
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
-from gql.transport.exceptions import TransportError
 
 from src.graphql import IDENTIFY_ACCOUNT_REQUEST
 
 
-class Client:
-    def __init__(self, base_uri=os.env.get('DASHX_BASE_URI', 'https://api.dashx.com/graphql'),
-                 public_key=os.env.get('DASHX_PUBLIC_KEY'), private_key=os.env.get('DASHX_PRIVATE_KEY'),
-                 target_environment=os.env.get('DASHX_TARGET_ENVIRONMENT')):
+class GqlClient:
+    def __init__(self, base_uri=os.environ.get('DASHX_BASE_URI', 'https://api.dashx.com/graphql'),
+                 public_key=os.environ.get('DASHX_PUBLIC_KEY'), private_key=os.environ.get('DASHX_PRIVATE_KEY'),
+                 target_environment=os.environ.get('DASHX_TARGET_ENVIRONMENT')):
         self.base_uri = base_uri
         self.public_key = public_key
         self.private_key = private_key
         self.target_environment = target_environment
 
-    def identify(self, user_id, first_name, last_name, email):
-        # Select your transport with a defined url endpoint
+    def identify(self, uid=None, **params):
+        q_params = dict()
+        if uid is None:
+            q_params["anonymousUid"] = str(uuid.uuid4())
+        else:
+            q_params["uid"] = str(uid)
+        q_params.update(params)
+
+        return self.make_request(q_params, IDENTIFY_ACCOUNT_REQUEST)
+
+    def make_request(self, q_params, query_name):
+        query = gql(query_name)
+
         transport = AIOHTTPTransport(url=self.base_uri, headers=self.generate_header())
 
-        # Create a GraphQL client using the defined transport
-        client = Client(transport=transport, fetch_schema_from_transport=True)
-        params = {"input": user_id} # To do: need to change this to take all the user information
-        query = gql(IDENTIFY_ACCOUNT_REQUEST)
-        try:
-            result = client.execute(query, variable_values=params)
-            return result
-        except TransportError:
-            raise Exception("GraphQL request to backend service unsuccessful")
+        client = Client(transport=transport, fetch_schema_from_transport=False)
+        result = client.execute(query, variable_values={'input': q_params})
+        return result
 
     def generate_header(self):
         return {
             'User-Agent': 'dashx-python',
-            'X-Public-Key': self.publicKey,
-            'X-Private-Key': self.privateKey,
-            'X-Target-Environment': self.targetEnvironment,
+            'X-Public-Key': self.public_key,
+            'X-Private-Key': self.private_key,
+            'X-Target-Environment': self.target_environment,
             'Content-Type': 'application/json'
         }
